ce3130f3815b06032fea8071b9f4337b7321f0d6
Luke Pickering <lp208@ic.ac.uk>
2016-03-04 14:47:49 +0000

    NUWRO CORE Changes: Adds dumpParams to src/CMakeLists.txt. Slight clean up of src/args.h.

--- src/args.h
+++ src/args.h
@@ -7,46 +7,48 @@
 using namespace std;
 
 
-class args
-{
+class args {
 public:
-const char * program;
-const char * input;
-const char * output;
-const char * progress;
-stringstream params;
 
-args(const char* p="nuwro",const char* i="params.txt",const char* o="eventsout.root"):
-  program(p),input(i),output(o)
-{}
+  const char * program;
+  const char * input;
+  const char * output;
+  const char * progress;
+  stringstream params;
 
-int read(int argc, char** argv)
-{ 
-  if(argc%2==0) 
+args(const char* p="nuwro",
+  const char* i="params.txt",
+  const char* o="eventsout.root") : program(p), input(i), output(o){}
+
+int read(int argc, char** argv){
+
+  if( (argc%2) == 0){
+    usage();
+  }
+
+  for(int i=1;i<argc-1;i++) {
+    if( (string(argv[i]) == "-i") and (argv[i+1][0] != '-') ){
+             input=argv[++i];
+    } else if( (string(argv[i]) == "-o") and (argv[i+1][0] != '-') ){
+             output=argv[++i];
+    } else if( (string(argv[i]) == "-progress") and (argv[i+1][0] != '-') ){
+             progress=argv[++i];
+    } else if( (string(argv[i]) == "-p") and (argv[i+1][0] != '-') ){
+      params << argv[++i] << endl;
+    }
+    else{
      usage();
-  for(int i=1;i<argc-1;i++)
-     {
-	if(string(argv[i])=="-i" and argv[i+1][0]!='-')
-           input=argv[++i];
-        else 
-	if(string(argv[i])=="-o" and argv[i+1][0]!='-')
-           output=argv[++i];
-        else 
-	if(string(argv[i])=="-progress" and argv[i+1][0]!='-')
-           progress=argv[++i];
-	else
-	if(string(argv[i])=="-p" and argv[i+1][0]!='-')
-           params<<argv[++i]<<endl;
-        else 
-	   usage();
-     } 
-   return 0;  
+    }
+  }
+  return 0;
 }
 
 void usage()
 {
-  cout <<"usage: "<< program <<" [-i input_paramaters_file] [-o output_root_file] [-p \"param1=value1\"] [-p \"param2=value2\"]...\n";
-  exit(22);  
+  cout << "usage: " << program
+    << " [-i input_paramaters_file] [-o output_root_file] [-p \"param1="
+    "value1\"] [-p \"param2=value2\"]...\n";
+  exit(22);
 }
 
 };
--- src/dumpParams.cc
+++ src/dumpParams.cc
@@ -1,14 +1,24 @@
-//#include <iostream>
-//#include "stdlib.h"
 #include "params.h"
 #include "args.h"
 #include "dirs.h"
 
+void usage(char const *argv0){
+  std::cout << "[USAGE]: " << argv0
+    << " [-i input_paramaters_file] [-p \"param1="
+    "value1\"] [-p \"param2=value2\"]...\n";
+}
+
 int main(int argc, char** argv){
 	set_dirs(argv[0]);
 	params p;
+
+  if( (argc%2) == 0){
+    usage(argv[0]);
+    return 22;
+  }
+
 	args a;
-        a.read (argc, argv);
+  a.read (argc, argv);
 	p.read (a.input);
 	p.read (a.params, "command line");
 	p.list ();
